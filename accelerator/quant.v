57764c7e4ee9493f31b7f0cd9a525311dd1fcea1
yasith46 <yasithudana3@gmail.com>
2025-06-07 12:45:40 +0530

    Did some changes to collector and the testbench, and added the unet model

--- accelerator/quant.v
+++
@@ -1,8 +0,0 @@
-module quant(
-		input  [31:0] in, bias, zeropoint, scaler_scaled16,
-		output [7:0]  result
-	);
-	
-	assign result = ((in + bias + zeropoint) * scaler_scaled16) >> 16;
-	
-endmodule 
